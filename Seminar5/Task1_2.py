99d5b81e7cba75125dcce28297a36a4ae2178ef9
Anna Lapshina <lapshina.anna0@ya.ru>
2023-01-20 21:39:58 +0300

    add solution of task 1_1, 1_2, 1_3 for seminar 5

---
+++ Seminar5/Task1_2.py
@@ -0,0 +1,45 @@
+# Создайте программу для игры с конфетами человек против человека.
+# Условие задачи: На столе лежит заданное количество конфет. Играют 
+# два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. 
+# За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются 
+# сделавшему последний ход.
+# a) Добавьте игру против бота
+
+
+from random import randint as ri
+
+def game(num: int, candy: int):
+    count = 1
+    while candy > 0:
+        taken = 0
+        if (count%2 != 0):
+            winner = num
+        else:
+            if num == 1:
+                winner = num + 1
+            else:
+                winner = num - 1
+        if winner == 2:
+            taken = ri(1, 28)
+            while taken > candy:
+                taken = ri(1, 28)
+            print(f'{count} ход. Бот забирает {taken} шт. конфет' )
+        else:
+            taken = int(input(f'{count} ход. Игрок № {winner} - выберите число конфет, которое хотите взять: ' ))
+        if (taken > 28) or (taken < 1):
+            print('Ты можешь взять только от 1 до 28 конфет за ход!')
+            break
+        else:
+            count += 1
+            candy -= taken
+            print(f'Количество конфет в игре = {candy}')
+        if candy == 0:
+         print(f'Конец игры. Игрок № {winner} забирает все конфеты!' )
+                 
+    return()
+
+
+num_player = ri(1,2)
+quantity = int(input('Введите число конфет: '))
+print(f'Игрок № 2 - бот. Первый ход за игроком № {num_player}')
+game(num_player, quantity)
